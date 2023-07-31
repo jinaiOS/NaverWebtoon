0257d67e94a72fcc744c574e384eb19744be9583
jingni1115 <kj227777@naver.com>
2023-07-31 09:09:57 +0900

    feat: Add Alamofire podfile

---
+++ NaverWebtoon/Pods/Target Support Files/Pods-NaverWebtoon-NaverWebtoonUITests/Pods-NaverWebtoon-NaverWebtoonUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NaverWebtoon_NaverWebtoonUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NaverWebtoon_NaverWebtoonUITests
+@end
---
+++ NaverWebtoon/Pods/Target Support Files/Pods-NaverWebtoon/Pods-NaverWebtoon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NaverWebtoon : NSObject
+@end
+@implementation PodsDummy_Pods_NaverWebtoon
+@end
